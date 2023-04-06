b18d141e39f20c6da09fb1004faa535d80381c23
Reactongraph <js.developer@ongraph.com>
2023-04-06 21:05:58 +0530

    Fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import SignupForm from "./Auth/signup";
 import LoginForm from "./Auth/login";
@@ -9,16 +9,16 @@ import { PrivateRoute } from "./PrivateRoute";
 const App = () => {
   return (
     <Container>
-      <HashRouter>
+      <Router>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/login" element={<LoginForm />} />
+          <Route exact path="/" element={<LoginForm />} />
+          <Route exact path="/signup" element={<SignupForm />} />
+          <Route exact path="/login" element={<LoginForm />} />
           <Route exact path="/" element={<PrivateRoute />}>
             <Route path="/article" element={<Article />} />
           </Route>
         </Routes>
-      </HashRouter>
+      </Router>
     </Container>
   );
 };
